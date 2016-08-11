54b8e4e120a4bc3e2cdfa9d2f99c245d67ecb1bc
Bruno Rodrigues <brunocroh@gmail.com>
2016-08-10 21:48:35 -0300

    Ativudade 2

--- ProgramacaoMultiPlataforma/src/ExercicioMatrizes/Exercicio2.java
+++ ProgramacaoMultiPlataforma/src/ExercicioMatrizes/Exercicio2.java
@@ -1,5 +1,24 @@
 package ExercicioMatrizes;
 
 public class Exercicio2 {
-    
+    public static void main(String[] args) {
+        int[] notasAlunos = new int[]{7,12,100,4,45,56,57,66,32,33,80,87,92,46,74,22};
+        int[] intervaloNotas = new int[11];
+        
+        for(int i = 0; i < notasAlunos.length; i++){
+            intervaloNotas[notasAlunos[i]/10]++;
+              
+        }
+        
+        for(int i = 0; i < intervaloNotas.length; i++){
+            if(i == intervaloNotas.length-1){
+                System.out.print("100 : "+intervaloNotas[i] );
+            }else{
+                System.out.printf("%02d-%02d: ", i*10, i*10+9);
+                System.out.printf(""+intervaloNotas[i]);
+            }
+            System.out.println("");
+        }
+        
+    }
 }
